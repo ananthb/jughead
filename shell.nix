ffc1d30453d58e812f90bb76864a11de655a5cc6
Ananth Bhaskararaman <antsub@gmail.com>
2025-04-24 08:19:49 +0530

    add devshell

--- shell.nix
+++ shell.nix
@@ -1,12 +1,18 @@
-{ pkgs ? import <nixpkgs> {} }:
+# Use `builtins.getFlake` if available
+if builtins ? getFlake then
+  let
+    scheme = if builtins.pathExists ./.git then "git+file" else "path";
+  in
+  (builtins.getFlake "${scheme}://${toString ./.}").devShells.${builtins.currentSystem}.default
 
-pkgs.mkShell {
-  name = "jughead";
-
-  buildInputs = with pkgs; [
-    go
-    hugo
-    nodePackages.prettier
-    prettier-plugin-go-template
-  ];
-}
+# Otherwise we'll use the flake-compat shim
+else
+  (import (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  ) { src = ./.; }).shellNix
