186efa43fd4c97b1bdcaaddcb59159fec5555e22
Ananth Bhaskararaman <antsub@gmail.com>
2026-02-08 15:29:17 +0530

    New colours and themes
    New colours and themes. Layout clean ups.

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "jughead development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.git
+              pkgs.go
+              pkgs.hugo
+            ];
+
+            shellHook = ''
+              echo "hello from nix develop"
+              git --version
+            '';
+          };
+        });
+    };
+}
